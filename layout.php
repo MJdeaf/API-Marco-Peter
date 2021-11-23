5e56ee95814fdc5f4186da7d848de74ec945aedf
pkluiber <pkluiber@gmail.com>
2021-11-23 14:20:02 +0100

    lol1

--- layout.php
+++ layout.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>API</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 
 </head>
 <body>
@@ -17,6 +18,25 @@
     </a>
   </div>
 </nav>
-    
+
+<h1>English</h1>
+<p>The apprentice saves his boss from drowning. Says the boss:
+"You have one wish for that! What do you wish for the most?"
+The apprentice thinks for a while and finally says:
+"Don't tell anyone in the factory that it was me who saved you..."</p>
+
+<h1>German</h1>
+<p>Der Lehrling rettet seinen Chef vor dem Ertrinken. Sagt der Chef:
+„Dafür hast du einen Wunsch frei! Was wünschst du dir am meisten?“
+Überlegt der Lehrling eine Weile und sagt schließlich:
+„Erzählen Sie in der Fabrik niemandem, dass ich es war, der Sie gerettet hat…“
+
+</p>
+
+<img class="icon" src="laughing.gif">
+
+
+
+
 </body>
 </html>
